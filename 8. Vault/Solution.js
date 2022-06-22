d40bae9e60d9faa6954aacf6f4f79c25e940fe37
Ege Aybars Bozkurt <egeaybars1102@gmail.com>
2022-06-22 22:31:29 +0300

    adding comments for vault

--- 8. Vault/Solution.js
+++ 8. Vault/Solution.js
@@ -4,3 +4,9 @@ contract.unlock(password);
 
 //checked if it is locked: false
 await contract.locked()
+
+/*It should be known that private variables are not secret; it
+just means that it cannot be read or used by other contracts.
+Variables are stored as slots in smart contracts, and the password 
+variable is stored in slot2 which could be read by web3.eth.getStorageAt.
+*/
