1423150fd7705c44abbba0e92d3d765e1c8b8ea8
egeaybars123 <egeaybars1102@gmail.com>
2022-06-29 12:20:38 +0300

    add preservation

---
+++ 16. Preservation/Preservation.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract Preservation {
+
+  // public library contracts 
+  address public timeZone1Library;
+  address public timeZone2Library;
+  address public owner; 
+  uint storedTime;
+  // Sets the function signature for delegatecall
+  bytes4 constant setTimeSignature = bytes4(keccak256("setTime(uint256)"));
+
+  constructor(address _timeZone1LibraryAddress, address _timeZone2LibraryAddress) public {
+    timeZone1Library = _timeZone1LibraryAddress; 
+    timeZone2Library = _timeZone2LibraryAddress; 
+    owner = msg.sender;
+  }
+ 
+  // set the time for timezone 1
+  function setFirstTime(uint _timeStamp) public {
+    timeZone1Library.delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
+  }
+
+  // set the time for timezone 2
+  function setSecondTime(uint _timeStamp) public {
+    timeZone2Library.delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
+  }
+}
+
+/*
+Using delegatecall for external contracts has some effects on storage.
+timeZone libraries are referring to a contract where only storage is uint256 time.
+Using delegatecall for this contract changes the value of the slot 0 storage which is
+timeZone1Library in this case. If we pass the address of a malicious contract, instead of a
+time, then we can capture ownership with a contract given in Attack.sol.
+
+*/
